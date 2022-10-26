bce8903dff9762c4ffeca50b9bcfd4bafeec4ee4
Mustafa Özdemir <62721417+musozd@users.noreply.github.com>
2022-10-26 21:52:31 +0200

    Setup

---
+++ main.js
@@ -0,0 +1,15 @@
+const hero = {
+  elementId: 'hero',
+  name: 'Wizard',
+  avatar: 'images/wizard.png',
+  health: 60,
+  diceRoll: 3
+};
+
+const monster = {
+  elementId: 'monster',
+  name: 'Orc',
+  avatar: 'images/orc.png',
+  health: 10,
+  diceRoll: 4
+};
