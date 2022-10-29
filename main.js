55547c7bebdd131178b45947c48153f0f13f4c03
Mustafa Özdemir <62721417+musozd@users.noreply.github.com>
2022-10-29 11:31:25 +0200

    Render dice with random numbers

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
+function getDiceRollArray(diceCount) {
+  let newDiceRolls = [];
+  for (let i = 0; i < diceCount; i++) {
+    newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
+  }
+  return newDiceRolls;
+}
+
+function getDiceHtml(diceCount) {
+  return getDiceRollArray(diceCount)
+    .map(function (num) {
+      return `<div class="dice">${num}</div>`;
+    })
+    .join('');
+}
+
 const hero = {
   elementId: 'hero',
   name: 'Wizard',
   avatar: 'images/wizard.png',
   health: 60,
-  diceRoll: 3
+  diceCount: 3
 };
 
 const monster = {
@@ -11,5 +27,22 @@ const monster = {
   name: 'Orc',
   avatar: 'images/orc.png',
   health: 10,
-  diceRoll: 4
+  diceCount: 1
 };
+
+function renderCharacter(data) {
+  const { elementId, name, avatar, health, diceCount } = data;
+  const diceHtml = getDiceHtml(diceCount);
+
+  document.getElementById(elementId).innerHTML = `<div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
+            <div class="dice-container">    
+                ${diceHtml}
+            </div>
+        </div>`;
+}
+
+renderCharacter(hero);
+renderCharacter(monster);
