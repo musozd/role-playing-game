7fcdc1ffebda82ae1429746b280ff9654caf7294
Mustafa Özdemir <62721417+musozd@users.noreply.github.com>
2022-11-27 22:53:41 +0100

    Update functions & add new characters

--- Character.js
+++ Character.js
@@ -1,30 +1,59 @@
-import { getDiceRollArray } from '/utils.js';
+import {
+  getDiceRollArray,
+  getDicePlaceholderHtml,
+  getPercentage
+} from './utils.js';
 
-function Character(data) {
-  Object.assign(this, data);
+class Character {
+  constructor(data) {
+    Object.assign(this, data);
+    this.maxHealth = this.health;
+    this.diceHtml = getDicePlaceholderHtml(this.diceCount);
+  }
 
-  this.getDiceHtml = function (diceCount) {
-    return getDiceRollArray(diceCount)
-      .map(function (num) {
-        return `<div class="dice">${num}</div>`;
-      })
+  setDiceHtml() {
+    this.currentDiceScore = getDiceRollArray(this.diceCount);
+    this.diceHtml = this.currentDiceScore
+      .map((num) => `<div class="dice">${num}</div>`)
       .join('');
-  };
+  }
 
-  this.getCharacterHtml = function () {
-    const { elementId, name, avatar, health, diceCount } = this;
-    let diceHtml = this.getDiceHtml(diceCount);
+  takeDamage(attackScoreArray) {
+    const totalAttackScore = attackScoreArray.reduce(
+      (total, num) => total + num
+    );
+    this.health -= totalAttackScore;
+    if (this.health <= 0) {
+      this.dead = true;
+      this.health = 0;
+    }
+  }
 
+  getHealthBarHtml() {
+    const percent = getPercentage(this.health, this.maxHealth);
+    return `<div class="health-bar-outer">
+                    <div class="health-bar-inner ${
+                      percent < 26 ? 'danger' : ''
+                    }" 
+                            style="width:${percent}%;">
+                    </div>
+                </div>`;
+  }
+
+  getCharacterHtml() {
+    const { elementId, name, avatar, health, diceCount, diceHtml } = this;
+    const healthBar = this.getHealthBarHtml();
     return `
             <div class="character-card">
                 <h4 class="name"> ${name} </h4>
                 <img class="avatar" src="${avatar}" />
                 <div class="health">health: <b> ${health} </b></div>
+                ${healthBar}
                 <div class="dice-container">
                     ${diceHtml}
                 </div>
             </div>`;
-  };
+  }
 }
 
 export default Character;
